19e89dbe881353b7e9616bf5424767e669c3fe0f
woohyuk0428 <whkang03@gmail.com>
2025-11-28 20:56:28 +0900

    fix/#2 : 오타 수정

--- src/main/java/com/gdg/backend/auth/exception/BadReqeustException.java
+++
@@ -1,7 +0,0 @@
-package com.gdg.backend.auth.exception;
-
-public class BadReqeustException extends RuntimeException {
-    public BadReqeustException(String message) {
-        super(message);
-    }
-}
