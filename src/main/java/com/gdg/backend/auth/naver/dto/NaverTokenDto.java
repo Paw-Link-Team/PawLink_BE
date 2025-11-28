4b9aeb831dd08727f755deb9e98e29bb8f4eaea5
woohyuk0428 <whkang03@gmail.com>
2025-11-28 16:31:23 +0900

    feat/#2 : naverTokenDto 구현

---
+++ src/main/java/com/gdg/backend/auth/naver/dto/NaverTokenDto.java
@@ -0,0 +1,27 @@
+package com.gdg.backend.auth.naver.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NaverTokenDto {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    @JsonProperty("error")
+    private String error;
+
+    @JsonProperty("error_description")
+    private String errorDescription;
+}
