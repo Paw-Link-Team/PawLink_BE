9a5be3fbff6f0828309e0f7077283a2b1a40dfa2
woohyuk0428 <whkang03@gmail.com>
2025-12-01 19:38:36 +0900

    fix/#2 : claims 변경

--- src/main/java/com/gdg/backend/auth/jwt/TokenProvider.java
+++ src/main/java/com/gdg/backend/auth/jwt/TokenProvider.java
@@ -74,11 +74,11 @@ public class TokenProvider {
     public Authentication getAuthentication(String accessToken) {
         Claims claims = parseClaims(accessToken);
 
-        if (claims.get("auth") == null) {
+        if (claims.get("role") == null) {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
 
-        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("role").toString().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
 
