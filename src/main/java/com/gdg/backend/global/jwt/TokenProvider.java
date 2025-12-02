0a537e29c62318bd7112e52e3fc5d2f8d980a1f3
woohyuk0428 <whkang03@gmail.com>
2025-12-02 21:52:26 +0900

    feat/#2: 카카오 네이버 통합

--- src/main/java/com/gdg/backend/global/jwt/TokenProvider.java
+++ src/main/java/com/gdg/backend/global/jwt/TokenProvider.java
@@ -1,7 +1,6 @@
 package com.gdg.backend.global.jwt;
 
 import com.gdg.backend.user.domain.User;
-import com.gdg.backend.auth.dto.kakao.KakaoTokenDto;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -12,6 +11,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -29,46 +29,39 @@ import java.util.stream.Collectors;
 @Component
 public class TokenProvider {
     private final Key key;
-    private final Long accessTokenValiditySeconds;
-    private final Long refreshTokenValiditySeconds;
+    @Getter
+    private final Integer accessTokenValiditySeconds;
+    @Getter
+    private final Integer refreshTokenValiditySeconds;
 
     public TokenProvider(@Value("${jwt.secret}") String secretKey,
-                         @Value("${jwt.access-token-validity-in-milliseconds}") long accessTokenValiditySeconds,
-                         @Value("${jwt.refresh-token-validity-in-milliseconds}") long refreshTokenValiditySeconds) {
+                         @Value("${jwt.access-token-validity-in-milliseconds}") Integer accessTokenValiditySeconds,
+                         @Value("${jwt.refresh-token-validity-in-milliseconds}") Integer refreshTokenValiditySeconds) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.accessTokenValiditySeconds = accessTokenValiditySeconds;
         this.refreshTokenValiditySeconds = refreshTokenValiditySeconds;
     }
 
-    public KakaoTokenDto createToken(User user, String getKakaoToken) {
+    public String createToken(User user, Integer seconds) {
         Long nowTime = new Date().getTime();
 
-        Date expiryDate = new Date(nowTime + accessTokenValiditySeconds);
-        Date refreshDate = new Date(nowTime + refreshTokenValiditySeconds);
+        Date expiryDate = new Date(nowTime + seconds);
 
-        String accessToken = Jwts.builder()
+        return Jwts.builder()
                 .setSubject(user.getId().toString()) // user email
                 .claim("role", user.getRole().name()) //role : role에 따라 변경
                 .setExpiration(expiryDate)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
 
-        String refreshToken = Jwts.builder()
-                .setSubject(user.getId().toString())
-                .setExpiration(refreshDate)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
+    public String accessToken(User user){
+        return createToken(user, accessTokenValiditySeconds);
+    }
 
-        return KakaoTokenDto.builder()
-                .tokenType("bearer ") // 토큰 타입은 bearer
-                .accessToken(accessToken) //accessToken저장
-                .idToken(getKakaoToken) //idToken을 카카오 서버에서 생성한 토큰
-                .expiresIn(accessTokenValiditySeconds / 1000) // accessToken 만료시간 / 초단위
-                .refreshToken(refreshToken) //refreshToken 저장
-                .refreshTokenExpiresIn(refreshTokenValiditySeconds / 1000) // refreshToken 만료시간 / 초단위
-                .scope(user.getRole().name()) // 역할
-                .build();
+    public String refreshToken(User user){
+        return createToken(user, refreshTokenValiditySeconds);
     }
 
     public Authentication getAuthentication(String accessToken) {
