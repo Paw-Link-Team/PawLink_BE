ebddfcfe20fd549514548cdb50b4522768d69e61
woohyuk0428 <whkang03@gmail.com>
2025-12-02 21:49:25 +0900

    feat/#2 : 네이버 로그인 구현(오류 있음)

--- src/main/java/com/gdg/backend/auth/controller/AuthController.java
+++ src/main/java/com/gdg/backend/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.gdg.backend.auth.controller;
 
+import com.gdg.backend.auth.dto.kakao.KakaoTokenDto;
+import com.gdg.backend.auth.dto.naver.NaverTokenDto;
 import com.gdg.backend.auth.service.NaverAuthService;
+import com.gdg.backend.global.exception.BedReqeustException;
 import com.gdg.backend.user.domain.Role;
 import com.gdg.backend.auth.service.KakaoAuthService;
-import com.gdg.backend.user.dto.TokenDto;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,24 +24,26 @@ public class AuthController {
 
     @Operation(summary = "카카오 로그인", description = "code : accessToken을 받습니다.\nstate : provider, walker, admin 3가지로 역할이 부여됩니다. 프론트는 제공자를 선택하게 만들어주세요.")
     @GetMapping("/callback/kakao")
-    public TokenDto kakaoLogin(@RequestParam("code") String code,
-                               @RequestParam("state") String state) {
+    public KakaoTokenDto kakaoLogin(@RequestParam("code") String code,
+                                    @RequestParam("part") String part) {
         String token = kakaoAuthService.getKakaoToken(code);
 
-        Role role = Role.valueOf(state.toUpperCase());
+        Role role = Role.valueOf(part.toUpperCase());
 
         return kakaoAuthService.loginOrSignUp(token, role);
     }
 
     @Operation(summary = "네이버 로그인", description = "")
     @GetMapping("/callback/naver")
-    public TokenDto naverLogin(@RequestParam("code") String code,
-                               @RequestParam("state")String state) {
-        String token = naverAuthService.getNaverToken(code);
+    public NaverTokenDto naverLogin(@RequestParam("code") String code,
+                                    @RequestParam("state") String state
+                                    ){
 
-        Role role = Role.valueOf(state.toUpperCase());
+        String token = naverAuthService.getNaverToken(code, state);
 
-        return naverAuthService.naverLoginOrSignUp(token, role);
+        //Role role = Role.valueOf(part.toUpperCase());
+
+        return naverAuthService.naverLoginOrSignUp(token, Role.ADMIN);
     }
 
 
