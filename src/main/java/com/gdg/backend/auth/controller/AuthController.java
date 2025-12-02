a5d941ff602a4a6940bac0610690f079b8d443be
woohyuk0428 <whkang03@gmail.com>
2025-12-02 20:41:57 +0900

    feat/#2 : 카카오 네이버 통합 컨트롤러 구현

--- src/main/java/com/gdg/backend/auth/controller/AuthController.java
+++ src/main/java/com/gdg/backend/auth/controller/AuthController.java
@@ -1,8 +1,9 @@
 package com.gdg.backend.auth.controller;
 
+import com.gdg.backend.auth.service.NaverAuthService;
 import com.gdg.backend.user.domain.Role;
-import com.gdg.backend.auth.dto.kakao.KakaoTokenDto;
 import com.gdg.backend.auth.service.KakaoAuthService;
+import com.gdg.backend.user.dto.TokenDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,21 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final KakaoAuthService kakaoAuthService;
+    private final NaverAuthService naverAuthService;
 
     @Operation(summary = "카카오 로그인", description = "code : accessToken을 받습니다.\nstate : provider, walker, admin 3가지로 역할이 부여됩니다. 프론트는 제공자를 선택하게 만들어주세요.")
     @GetMapping("/callback/kakao")
-    public KakaoTokenDto kakaoLogin(@RequestParam("code") String code,
-                                    @RequestParam("state") String state) {
+    public TokenDto kakaoLogin(@RequestParam("code") String code,
+                               @RequestParam("state") String state) {
         String token = kakaoAuthService.getKakaoToken(code);
 
         Role role = Role.valueOf(state.toUpperCase());
 
         return kakaoAuthService.loginOrSignUp(token, role);
     }
-//
-//    @Operation(summary = "네이버 로그인", description = "")
-//    @GetMapping("/callback/naver")
-//    public KakaoTokenDto naverLogin(@RequestParam("code") String code) {}
+
+    @Operation(summary = "네이버 로그인", description = "")
+    @GetMapping("/callback/naver")
+    public TokenDto naverLogin(@RequestParam("code") String code,
+                               @RequestParam("state")String state) {
+        String token = naverAuthService.getNaverToken(code);
+
+        Role role = Role.valueOf(state.toUpperCase());
+
+        return naverAuthService.naverLoginOrSignUp(token, role);
+    }
 
 
 }
