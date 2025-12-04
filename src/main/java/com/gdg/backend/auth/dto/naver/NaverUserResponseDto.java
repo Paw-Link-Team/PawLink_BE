34a9ccdf89b66b29dd77cb313e6648c309aa69b0
woohyuk0428 <whkang03@gmail.com>
2025-12-04 19:42:25 +0900

    feat/#2: 생성자 접근 권한 변경

--- src/main/java/com/gdg/backend/auth/dto/naver/NaverUserResponseDto.java
+++ src/main/java/com/gdg/backend/auth/dto/naver/NaverUserResponseDto.java
@@ -2,11 +2,12 @@ package com.gdg.backend.auth.dto.naver;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NaverUserResponseDto {
     @JsonProperty("resultcode")
@@ -19,7 +20,8 @@ public class NaverUserResponseDto {
     private Response response;
 
     @Getter
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PUBLIC)
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Response {
         @JsonProperty("id")
         private String id;
