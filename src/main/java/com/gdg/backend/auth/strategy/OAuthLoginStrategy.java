b6f27c51817939daa0d77b508b45a7f48085e7d2
woohyuk0428 <whkang03@gmail.com>
2025-12-14 01:59:33 +0900

    Feat/#6: 불필요한 파일 제거

--- src/main/java/com/gdg/backend/auth/strategy/OAuthLoginStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.gdg.backend.auth.strategy;
-
-import com.gdg.backend.global.oauth.dto.UserInfoDto;
-import com.gdg.backend.user.domain.Provider;
-
-public interface OAuthLoginStrategy {
-    UserInfoDto getUserInfo(String accessToken);
-    Provider getProvider();
-}
