eb8a32b39d693cce2278f9c34dbfcd4af079e0ba
woohyuk0428 <whkang03@gmail.com>
2025-12-15 19:09:44 +0900

    Feat/#6: accessToken으로 접근제한

--- src/main/java/com/gdg/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/gdg/backend/global/config/SwaggerConfig.java
@@ -1,10 +1,25 @@
 package com.gdg.backend.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Pawlink API", version = "v1"),
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
@@ -12,6 +27,14 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("Auth API")
                 .pathsToMatch("/api/**","/login","/mypage/**","/signup")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi internalApi() {
+        return GroupedOpenApi.builder()
+                .group("Internal API")
+                .pathsToMatch("/internal/**", "/admin/**")
                 .build();
     }
 }
