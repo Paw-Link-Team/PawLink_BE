90201b07b319fe4b0107a9b1122c697241c54dcc
woohyuk0428 <whkang03@gmail.com>
2025-12-15 19:07:52 +0900

    Feat/#6: 관리자 accessToken 발급

--- src/main/java/com/gdg/backend/admin/service/AdminService.java
+++ src/main/java/com/gdg/backend/admin/service/AdminService.java
@@ -1,10 +1,13 @@
 package com.gdg.backend.admin.service;
 
 import com.gdg.backend.admin.dto.AdminSignupRequest;
-import com.gdg.backend.global.code.ErrorCode;
 import com.gdg.backend.global.exception.UserNotFoundException;
+import com.gdg.backend.global.jwt.TokenProvider;
+import com.gdg.backend.user.domain.Provider;
 import com.gdg.backend.user.domain.Role;
+import com.gdg.backend.user.domain.Type;
 import com.gdg.backend.user.domain.User;
+import com.gdg.backend.user.dto.TokenResponseDto;
 import com.gdg.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,19 +18,34 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
 
     @Transactional
-    public void createAdmin(AdminSignupRequest request){
+    public TokenResponseDto createAdmin(AdminSignupRequest request){
 
         User user = userRepository.findByProviderAndProviderId(
                 request.getProvider(),
                 request.getProviderId()
         ).orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다."));
 
-        if(user.getRole() == Role.SUPER_ADMIN){
-            return;
+        if(user.getRole() != Role.SUPER_ADMIN){
+            user.updateRole(Role.ADMIN);
         }
+        user.updateType(Type.ADMIN);
 
-        user.updateRole(Role.ADMIN);
+        String accessToken = tokenProvider.accessToken(user);
+        String refreshToken = tokenProvider.refreshToken(user);
+
+        return TokenResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .userId(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfileImageUrl())
+                .provider(user.getProvider())
+                .role(user.getRole())
+                .type(user.getType())
+                .build();
     }
 }
