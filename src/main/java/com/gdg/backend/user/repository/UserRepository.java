518232180088c482775e62b8a8996f17c39abf99
woohyuk0428 <whkang03@gmail.com>
2025-12-15 10:44:26 +0900

    Feat/#6: 불필요한 method 제거

--- src/main/java/com/gdg/backend/user/repository/UserRepository.java
+++ src/main/java/com/gdg/backend/user/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
     Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
     boolean existsByProviderAndProviderId(Provider provider, String providerId);
 }
