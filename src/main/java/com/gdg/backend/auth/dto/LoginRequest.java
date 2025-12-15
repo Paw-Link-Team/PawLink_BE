74fde05ebb7810d542f118aba472bbac73b0f5e6
woohyuk0428 <whkang03@gmail.com>
2025-12-15 10:47:54 +0900

    Feat/#6: 불필요한 import 제거

--- src/main/java/com/gdg/backend/auth/dto/LoginRequest.java
+++ src/main/java/com/gdg/backend/auth/dto/LoginRequest.java
@@ -2,7 +2,6 @@ package com.gdg.backend.auth.dto;
 
 import com.gdg.backend.user.domain.Provider;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public class LoginRequest {
