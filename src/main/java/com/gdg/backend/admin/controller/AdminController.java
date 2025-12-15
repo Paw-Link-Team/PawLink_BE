7ec9da2b667fe49bc5c316b89ff8910e00b77b03
woohyuk0428 <whkang03@gmail.com>
2025-12-15 19:07:34 +0900

    Feat/#6: PatchRequest 추가하여 경로명 설정

--- src/main/java/com/gdg/backend/admin/controller/AdminController.java
+++ src/main/java/com/gdg/backend/admin/controller/AdminController.java
@@ -4,26 +4,33 @@ import com.gdg.backend.admin.dto.AdminSignupRequest;
 import com.gdg.backend.admin.service.AdminService;
 import com.gdg.backend.global.code.SuccessCode;
 import com.gdg.backend.global.response.ApiResponse;
+import com.gdg.backend.user.dto.TokenResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/internal/admin")
+@RequestMapping("/internal")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole=('SUPER_ADMIN')")
+@PreAuthorize("hasAuthority('SUPER_ADMIN')")
+@Tag(name = "관리자")
 public class AdminController {
 
     private final AdminService adminService;
 
+    @PatchMapping("/admin")
+    @Operation(summary = "관리자 생성", description = "SUPER_ADMIN만 접속하여 관리자를 생성할 수 있습니다.")
     public ResponseEntity<ApiResponse<Object>> createAdmin(
             @RequestBody AdminSignupRequest request
     ){
-            adminService.createAdmin(request);
+            TokenResponseDto token = adminService.createAdmin(request);
 
-            return ApiResponse.success(SuccessCode.ADMIN_CREATED);
+            return ApiResponse.success(SuccessCode.ADMIN_CREATED, token);
     }
 }
