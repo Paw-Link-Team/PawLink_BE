1c79e54d84b2f315369e7af48931ac561514d3ac
woohyuk0428 <whkang03@gmail.com>
2025-12-15 10:54:53 +0900

    Feat/#6: 불필요한 import 제거

--- src/main/java/com/gdg/backend/auth/dto/SignupRequest.java
+++ src/main/java/com/gdg/backend/auth/dto/SignupRequest.java
@@ -1,7 +1,6 @@
 package com.gdg.backend.auth.dto;
 
 import com.gdg.backend.user.domain.Provider;
-import com.gdg.backend.user.domain.Role;
 import com.gdg.backend.user.domain.Type;
 import lombok.Getter;
 
