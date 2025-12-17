0913863da126427dee6da223b87346a31d17def4
Park, Juwon <won030702@gmail.com>
2025-12-18 00:43:57 +0900

    feat/#7: Dto 설계

---
+++ src/main/java/com/gdg/backend/board/dto/BoardRequestDto.java
@@ -0,0 +1,7 @@
+package com.gdg.backend.board.dto;
+
+public class BoardRequestDto {
+    private String title;
+    private String information;
+    private String description;
+}
---
+++ src/main/java/com/gdg/backend/board/dto/BoardResponseDto.java
@@ -0,0 +1,8 @@
+package com.gdg.backend.board.dto;
+
+public class BoardResponseDto {
+    private Long id;
+    private String title;
+    private String information;
+    private String description;
+}
