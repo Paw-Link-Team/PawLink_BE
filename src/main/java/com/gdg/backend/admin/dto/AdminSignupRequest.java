01e5c311666b836c7fe8591627a91b27c5945d8c
woohyuk0428 <whkang03@gmail.com>
2025-12-17 20:31:26 +0900

    feat/#6: idToken값에 저장으로 인하여 리팩토링

--- src/main/java/com/gdg/backend/admin/dto/AdminSignupRequest.java
+++ src/main/java/com/gdg/backend/admin/dto/AdminSignupRequest.java
@@ -1,10 +1,8 @@
 package com.gdg.backend.admin.dto;
 
-import com.gdg.backend.user.domain.OauthProvider;
 import lombok.Getter;
 
 @Getter
 public class AdminSignupRequest {
-    private OauthProvider oauthProvider;
-    private String providerId;
+    private String idToken;
 }
