e848396d180990d95c658a2afc37b4985e032a79
woohyuk0428 <whkang03@gmail.com>
2025-12-18 21:46:54 +0900

    feat/#12: default 이미지 url

---
+++ src/main/java/com/gdg/backend/user/image/profile/service/ProfileImageConstants.java
@@ -0,0 +1,10 @@
+package com.gdg.backend.user.image.profile.service;
+
+public final class ProfileImageConstants {
+
+    public static final String DEFAULT_PROFILE_IMAGE =
+            "https://pawlink-profile-images.s3.ap-northeast-2.amazonaws.com/profile/default.png";
+
+    private ProfileImageConstants() {
+    }
+}
