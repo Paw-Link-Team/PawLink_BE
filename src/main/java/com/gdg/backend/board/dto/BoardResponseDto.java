493d952c7143351165bcfc92144d1d3b78a88869
Park, Juwon <won030702@gmail.com>
2025-12-19 11:47:45 +0900

    fix/#7: Lombok어노테이션 추가

--- src/main/java/com/gdg/backend/board/dto/BoardResponseDto.java
+++ src/main/java/com/gdg/backend/board/dto/BoardResponseDto.java
@@ -1,11 +1,13 @@
 package com.gdg.backend.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class BoardResponseDto {
     private Long id;
     private String title;
     private String information;
     private String description;
-
-    public BoardResponseDto(Long id, String title, String information, String description) {
-    }
 }
