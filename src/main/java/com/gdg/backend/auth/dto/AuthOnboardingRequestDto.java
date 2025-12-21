99187062026f0606bc4428d1b364f57a06194c4e
woohyuk0428 <whkang03@gmail.com>
2025-12-21 15:32:49 +0900

    feat/#14: 기존 AuthRequestDto를 변경

--- src/main/java/com/gdg/backend/auth/dto/AuthOnboardingRequestDto.java
+++ src/main/java/com/gdg/backend/auth/dto/AuthOnboardingRequestDto.java
@@ -1,11 +1,16 @@
 package com.gdg.backend.auth.dto;
 
 import com.gdg.backend.user.domain.Type;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
-public class AuthRequestDto {
+public class AuthOnboardingRequestDto {
+    @NotBlank
     private String idToken;
+    @NotBlank
     private String nickname;
+    @NotNull
     private Type type;
 }
