4354bbfc869d88d79192a81d5fc7e5ead8516802
woohyuk0428 <whkang03@gmail.com>
2025-12-24 01:45:40 +0900

    feat/#23: walkHistory 정보 표현할 dto 구현

---
+++ src/main/java/com/gdg/backend/walkHistory/repository/WalkHistoryRepository.java
@@ -0,0 +1,12 @@
+package com.gdg.backend.walkHistory.repository;
+
+import com.gdg.backend.walkHistory.domain.WalkHistory;
+import com.gdg.backend.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WalkHistoryRepository extends JpaRepository<WalkHistory, Long> {
+
+    List<WalkHistory> findByUserOrderByStartedAtDesc(User user);
+}
